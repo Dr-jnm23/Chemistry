87309e07c42b7ebf8d295340ab8318273b24e49a
James Martin <63636440+Dr-jnm23@users.noreply.github.com>
2020-04-17 20:57:56 +0100

    Update mol_wgt.py
    cleaned up quotes

--- mol_wgt.py
+++ mol_wgt.py
@@ -1,15 +1,17 @@
-xxx = ["H", "C", "O"]
+# caclulate the molecular weight of a carbohydrate
+
+xxx = ['H', 'C', 'O']
 elements = set(xxx)
 while(True):
-    chemical_formula = input("Enter chemical formula, or enter to quit: ")
-    if chemical_formula == "":
+    chemical_formula = input('Enter chemical formula, or enter to quit: ')
+    if chemical_formula == '':
         break
     else:
         characters = list(chemical_formula)
         n = 0
         print(characters)
         for i in characters:
-            if characters[n] == "C":
+            if characters[n] == 'C':
                 c = 12.0107
                 if elements.intersection(set(characters[n+1])):
                     print(c)
@@ -17,7 +19,7 @@ while(True):
                     number = int(characters[n+1])
                     print(number*c)
 
-            elif characters[n] == "H":
+            elif characters[n] == 'H':
                 h = 1.00794
                 if elements.intersection(set(characters[n+1])):
                     print(h)
@@ -25,7 +27,7 @@ while(True):
                     number = int(characters[n+1])
                     print(number*h)
 
-            elif characters[n] == "O":
+            elif characters[n] == 'O':
                 o = 15.9994
                 if elements.intersection(set(characters[n+1])):
                     print(c)
